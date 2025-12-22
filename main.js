0824a38814e9afca1526048024eb552782a0f5f2
Burlington <perezuburlington@gmail.com>
2025-12-22 09:47:02 +0200

    mobile menu js added

---
+++ main.js
@@ -0,0 +1,7 @@
+const navbarMenu = document.querySelector('.nav-items');
+const navbarToggle = document.querySelector('mobile');
+
+navbarToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('active');
+    navbarToggle.classList.toggle('active');
+})
